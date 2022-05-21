9d9584092a89320918c4e4645d80be5b1e807524
Valeria Chinchilla <vchinchilla02@hotmail.com>
2022-05-21 15:14:11 -0600

    modulo en sintax y evalProp terminado

--- evalProp.hs
+++ evalProp.hs
@@ -1,6 +1,64 @@
-main :: IO ()    
-main = return()
+module EvalProp where
+    import Control.Exception  
+    
+    -- Para correr en VSCODE: agregar la sintaxis a este archivo 
+    -- y comentar la linea donde se importa Sintax
+    -- Importar modulo sintaxis
+    import Sintax
 
-data Identificador = String;
+    data Identificador = String;
 
-type Ambiente a b = [(Identificador,b)]  
+    -- Lista de tuplas
+    type Ambiente a b = [(Identificador,b)]  
+
+    {-
+    Funcion que devuelve el valor de la llave que recibe 
+    si es encontrado.
+    - Input: LLave
+    - Output:Valor de la llave
+    -}
+    busca :: (Eq x) => x -> [(x,y)] -> y
+    busca _ [] = error "Elemento no encontrado" 
+    busca z ((x,y):xs) = if z == x then y else busca z xs 
+
+    {-
+    Funcion que evalua una proposicion segun un ambiente
+    - Input: Un ambiente y una proposicion
+    - Output: El valor booleano resultante de la evaluacion
+    -}
+    evalProp ambiente prop = 
+        case prop of 
+        Constante valor -> valor
+        Variable var -> busca var ambiente
+        Negacion prop1 -> not (evalProp ambiente prop1)
+        Conjuncion prop1 prop2 ->
+            let valor1 = evalProp ambiente prop1
+                valor2 = evalProp ambiente prop2
+            in  valor1 && valor2
+        Disyuncion prop1 prop2 ->
+            let valor1 = evalProp ambiente prop1
+                valor2 = evalProp ambiente prop2
+            in  valor1 || valor2
+        Implicacion prop1 prop2 ->
+            let valor1 = evalProp ambiente prop1
+                valor2 = evalProp ambiente prop2
+            in
+                case (valor1, valor2) of
+                    (True, False) -> False  
+                    _ -> True         
+        Equivalencia prop1 prop2 ->
+            let valor1 = evalProp ambiente prop1
+                valor2 = evalProp ambiente prop2
+            in valor1 == valor2   
+
+    main :: IO ()  
+    main = do
+        -- prueba busca 
+        print(busca "a" [("a", True), ("b", True)])
+        -- prubea evalProp
+        let vp = Variable "p" 
+            vq = Variable "q" 
+            vr = Variable "r" 
+            prop1 = vp .&& vq .=> vq .|| vp .=> vp .&& vp
+            amb1 = [("p", True), ("q", False)]
+        print(evalProp amb1 prop1)
--- sintax.hs
+++ sintax.hs
@@ -1,63 +1,65 @@
-data Proposicion = 
-    Constante Bool 
-    | Variable String  
-    | Negacion Proposicion 
-    | Conjuncion Proposicion Proposicion 
-    | Disyuncion Proposicion Proposicion 
-    | Implicacion Proposicion Proposicion 
-    | Equivalencia Proposicion Proposicion 
-;
-imprimir :: Proposicion -> String;
+module Sintax where
+    data Proposicion = 
+        Constante Bool 
+        | Variable String  
+        | Negacion Proposicion 
+        | Conjuncion Proposicion Proposicion 
+        | Disyuncion Proposicion Proposicion 
+        | Implicacion Proposicion Proposicion 
+        | Equivalencia Proposicion Proposicion 
+    ;
+    imprimir :: Proposicion -> String;
 
-imprimir prop = 
-    case prop of 
-      Constante False -> "false"
-      Constante True -> "true" 
-      Variable nombre -> nombre
-      Negacion prop1 -> "negacion (" ++ imprimir  prop1 ++ ")"
-      Conjuncion prop1 prop2 -> "conjuncion (" ++ (imprimir prop1) ++ ", " ++ imprimir prop2 ++ ")"
-      Disyuncion prop1 prop2 -> "disyuncion (" ++ imprimir prop1 ++ ", " ++ imprimir prop2 ++ ")"
-      Implicacion prop1 prop2 -> "implicacion (" ++ imprimir prop1 ++ ", " ++ imprimir prop2 ++ ")"
-      Equivalencia prop1 prop2 -> "equivalencia (" ++ imprimir prop1 ++ ", " ++ imprimir prop2 ++ ")"
-;
+    imprimir prop = 
+        case prop of 
+        Constante False -> "false"
+        Constante True -> "true" 
+        Variable nombre -> nombre
+        Negacion prop1 -> "negacion (" ++ imprimir  prop1 ++ ")"
+        Conjuncion prop1 prop2 -> "conjuncion (" ++ (imprimir prop1) ++ ", " ++ imprimir prop2 ++ ")"
+        Disyuncion prop1 prop2 -> "disyuncion (" ++ imprimir prop1 ++ ", " ++ imprimir prop2 ++ ")"
+        Implicacion prop1 prop2 -> "implicacion (" ++ imprimir prop1 ++ ", " ++ imprimir prop2 ++ ")"
+        Equivalencia prop1 prop2 -> "equivalencia (" ++ imprimir prop1 ++ ", " ++ imprimir prop2 ++ ")"
+    ;
 
-(.~) :: Proposicion -> Proposicion
-(.~) = Negacion
+    (.~) :: Proposicion -> Proposicion
+    (.~) = Negacion
 
-infix 7 .&&
-(.&&) :: Proposicion -> Proposicion -> Proposicion
-(.&&) = Conjuncion
+    infix 7 .&&
+    (.&&) :: Proposicion -> Proposicion -> Proposicion
+    (.&&) = Conjuncion
 
-infix 6 .||
-(.||) :: Proposicion -> Proposicion -> Proposicion
-(.||) = Disyuncion
+    infix 6 .||
+    (.||) :: Proposicion -> Proposicion -> Proposicion
+    (.||) = Disyuncion
 
-infixr 5 .=>
-(.=>) :: Proposicion -> Proposicion -> Proposicion
-(.=>) = Implicacion
+    infixr 5 .=>
+    (.=>) :: Proposicion -> Proposicion -> Proposicion
+    (.=>) = Implicacion
 
-infix 4 .<=>
-(.<=>) :: Proposicion -> Proposicion -> Proposicion
-(.<=>) = Equivalencia
+    infix 4 .<=>
+    (.<=>) :: Proposicion -> Proposicion -> Proposicion
+    (.<=>) = Equivalencia
 
-main :: IO ()    
-main = do
-    let pru1 = (Variable "a") .<=> (Variable "b")
-    let pru2 = (Variable "a") .&& (Variable "b")
-    let pru3 = pru1 .|| pru2
-    let pru4 = (.~) (Variable "a")
-    print(imprimir(pru1))
-    print(imprimir(pru2))
-    print(imprimir(pru3))
-    print(imprimir(pru4))
+    main :: IO ()    
+    main = do
+        -- pruebas con variables
+        let pru1 = (Variable "a") .<=> (Variable "b")
+            pru2 = (Variable "a") .&& (Variable "b")
+            pru3 = pru1 .|| pru2
+            pru4 = (.~) (Variable "a")
+        print(imprimir(pru1))
+        print(imprimir(pru2))
+        print(imprimir(pru3))
+        print(imprimir(pru4))
 
-    -- pruebas con constantes
-    let f = Constante False;
-    let t = Constante True;
-    let p = f;
-    let q = t;
-    let prop1 = p .=> q .<=> p .|| q
-    let prop2 = p .=> q .<=> (.~) q .=> (.~) p
-    print(imprimir(prop1))
-    print(imprimir(prop2))
-    
+        -- pruebas con constantes
+        let f = Constante False
+            t = Constante True
+            p = f
+            q = t
+            prop1 = p .=> q .<=> p .|| q
+            prop2 = p .=> q .<=> (.~) q .=> (.~) p
+        print(imprimir(prop1))
+        print(imprimir(prop2))
+        
