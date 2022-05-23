81a16447da2972e7549a2d38f1a4c9f4604a7571
Valeria Chinchilla <vchinchilla02@hotmail.com>
2022-05-23 11:36:40 -0600

    Simpl Final + Pruebas

--- CodigoFuente/simpl.hs
+++ CodigoFuente/simpl.hs
@@ -16,61 +16,44 @@ module Simpl where
             Conjuncion p1 p2 -> (.~)p1 .|| (.~)p2
             -- REGLA 3: DE MORGAN ~(P v Q) <=> ~P ^ ~Q
             Disyuncion p1 p2 -> (.~)p1 .&& (.~)p2
-            _ -> prop 
+            _ -> prop   
         Conjuncion prop1 prop2 -> 
-            -- Evaluar el lado izquierdo
-            case prop1 of
-            Variable p1 ->
-                -- Evaluar el lado derecho
-                case prop2 of
-                -- REGLA 4: INVERSOS P ^ ~P <=> F
-                Negacion p2 -> if p1 == imprimir(p2) then (Constante False) else prop
-                -- REGLA 5 y 6: 
-                --   NEUTRO P ^ V <=> P 
-                --   DOMINACION P ^ F <=> F
-                Constante p2 -> if p2 == True then (Variable p1) else (Constante False)
-                -- REGLA 7: IDEMPOTENCIA P ^ P <=> P
-                Variable p2 -> if p1 == p2 then Variable p1 else prop
-                -- REGLA 8 y 9: 
-                --   DISTRIBUTIVA P ^ ( Q  v R ) <=>  (P ^  Q) v (P ^ R)
-                --   ABSORCION P ^ (P v Q) <=> P
-                Disyuncion p2 p3 -> 
-                    case p2 of
-                    Variable p21 -> 
-                        case p3 of  
-                        Variable p31 -> if p1 == imprimir(p2) then Variable p1 else ((prop1 .&& p2) .|| (prop1 .&& p3))
-                        _ -> prop1 .&& unaSimp(unaSimp p2 .|| unaSimp p3)
-                    _ -> prop1 .&& unaSimp(unaSimp p2 .|| unaSimp p3)
-                _ -> prop1 .&& unaSimp(unaSimp prop1 .&& unaSimp prop2)
-            _ -> unaSimp prop1 .&& unaSimp prop2    
+            -- Evaluar el lado derecho
+            case prop2 of
+            -- REGLA 4: INVERSOS P ^ ~P <=> F
+            Negacion p2 -> if imprimir(prop1) == imprimir(p2) then (Constante False) else unaSimp prop1 .&& unaSimp prop2
+            -- REGLA 5 y 6: 
+            --   NEUTRO P ^ V <=> P 
+            --   DOMINACION P ^ F <=> F
+            Constante p2 -> if p2 == True then prop1 else (Constante False)
+            -- REGLA 7: IDEMPOTENCIA P ^ P <=> P
+            Variable p2 -> if imprimir(prop1) == imprimir(prop2) then prop1 else unaSimp prop1 .&& unaSimp prop2
+            -- REGLA 8 y 9: 
+            --   DISTRIBUTIVA P ^ ( Q  v R ) <=>  (P ^  Q) v (P ^ R)
+            --   ABSORCION P ^ (P v Q) <=> P
+            Disyuncion p2 p3 -> if imprimir(prop1) == imprimir(p2) then prop1 else (unaSimp prop1 .&& unaSimp p2) .|| (unaSimp prop1 .&& unaSimp p3)
+            _ -> unaSimp prop1 .&& unaSimp prop2
                     
         Disyuncion prop1 prop2 ->
-            case prop1 of
-            Variable p1 ->
-                -- Evaluar el lado derecho
-                case prop2 of
-                -- REGLA 10: INVERSOS P v ~P <=> T
-                Negacion p2 -> if p1 == imprimir(p2) then (Constante True) else prop
-                -- REGLA 11 y 12: 
-                --   NEUTRO P v F <=> P 
-                --   DOMINACION P v T <=> V
-                Constante p2 -> if p2 == False then (Variable p1) else (Constante True)
-                -- REGLA 13: IDEMPOTENCIA P v P <=> P
-                Variable p2 -> if p1 == p2 then Variable p1 else prop
-                -- REGLA 14 y 15: 
-                --   DISTRIBUTIVA P v ( Q ^ R ) <=>  (P v  Q) ^ (P v R)
-                --   ABSORCION P v (P ^ Q) <=> P
-                Conjuncion p2 p3 -> 
-                    case p2 of
-                    Variable p21 -> 
-                        case p3 of  
-                        Variable p31 -> if p1 == imprimir(p2) then Variable p1 else ((prop1 .|| p2) .&& (prop1 .|| p3))
-                        _ -> prop1 .|| unaSimp(unaSimp p2 .&& unaSimp p3)
-                    _ -> prop1 .|| unaSimp(unaSimp p2 .&& unaSimp p3)
-                _ -> prop1 .|| unaSimp(unaSimp prop1 .|| unaSimp prop2)
+            -- Evaluar el lado derecho
+            case prop2 of
+            -- REGLA 10: INVERSOS P v ~P <=> T
+            Negacion p2 -> if imprimir(prop1) == imprimir(p2) then (Constante True) else unaSimp prop1 .|| unaSimp prop2
+            -- REGLA 11 y 12: 
+            --   NEUTRO P v F <=> P 
+            --   DOMINACION P v T <=> V
+            Constante p2 -> if p2 == False then prop1 else (Constante True)
+            -- REGLA 13: IDEMPOTENCIA P v P <=> P
+            Variable p2 -> if imprimir(prop1) == imprimir(prop2) then prop1 else unaSimp prop1 .|| unaSimp prop2
+            -- REGLA 14 y 15: 
+            --   DISTRIBUTIVA P v ( Q ^ R ) <=>  (P v  Q) ^ (P v R)
+            --   ABSORCION P v (P ^ Q) <=> P
+            Conjuncion p2 p3 -> if imprimir(prop1) == imprimir(p2) then prop1 else unaSimp prop1 .|| unaSimp(unaSimp p2 .&& unaSimp p3)
+
             _ -> unaSimp prop1 .|| unaSimp prop2
+
         --- REGLA 16: IMPLICACION DISYUNCION P => Q <=> ~P v Q
-        Implicacion prop1 prop2 -> (.~) prop1 .|| prop2
+        Implicacion prop1 prop2 -> (.~)(unaSimp prop1) .|| (unaSimp prop2)
         _ -> prop
     ;
 
--- CodigoFuente/simpl_pruebas.hs
+++ CodigoFuente/simpl_pruebas.hs
@@ -11,11 +11,64 @@ main = do
         t = Constante True
         ne = (.~) p
         neq = (.~) q
-        prop = p .|| ((q .|| neq) .&& r)
-        prop2 = p .|| (r .&& (q .|| neq) )
-        prop3 = (q .|| neq) .|| (r .&& p)
-        prop4 = p .&& ((q .&& neq) .|| r)
+        -- Para probas inversos y neutro 
+        prop = p .|| (r .&& (q .|| neq) ) 
+        prop2 = p .&& (r .|| (q .&& neq))
+        -- Para probar doble negacion 
+        prop3 = (.~)((.~)(q .|| p))
+        -- Para probar de morgan
+        prop4 = (.~)((q .|| p) .&& (r .|| p))
+        -- Para probar dominacion
+        prop5 = p .&& f
+        -- Para probar distributiva e idemportencia 
+        prop6 = p .&& ((q .|| q) .|| (r .&& r))
+        -- Para probar abstraccion e inversos
+        prop7 = (q .|| (q .&& neq)) .|| p 
+        -- Para probar implicacion disyuncion
+        prop8 = p .=> (q .=> r) 
+        -- Para probar absorcion
+        prop9 = (q .|| r) .|| ((q .|| r) .&& p)  
+    putStr("\n------Prop1------\n")
+    putStr(imprimir(prop))
+    putStr("------Simplificacion------\n")
     putStr(bonita(simpl prop))
+    
+    putStr("\n\n------Prop2------\n")
+    putStr(bonita prop2)
+    putStr("------Simplificacion------\n")
     putStr(bonita(simpl prop2))
+    
+    putStr("\n\n------Prop3------\n")
+    putStr(bonita prop3)
+    putStr("------Simplificacion------\n")
     putStr(bonita(simpl prop3))
+    
+    putStr("\n\n------Prop4------\n")
+    putStr(bonita prop4)
+    putStr("------Simplificacion------\n")
     putStr(bonita(simpl prop4))
+
+    putStr("\n\n------Prop5------\n")
+    putStr(bonita prop5)
+    putStr("------Simplificacion------\n")
+    putStr(bonita(simpl prop5))
+
+    putStr("\n\n------Prop6------\n")
+    putStr(bonita prop6)
+    putStr("------Simplificacion------\n")
+    putStr(bonita(simpl prop6))
+
+    putStr("\n\n------Prop7------\n")
+    putStr(bonita prop7)
+    putStr("------Simplificacion------\n")
+    putStr(bonita(simpl prop7))
+
+    putStr("\n\n------Prop8------\n")
+    putStr(bonita prop8)
+    putStr("------Simplificacion------\n")
+    putStr(bonita(simpl prop8))
+
+    putStr("\n\n------Prop9------\n")
+    putStr(bonita prop9)
+    putStr("------Simplificacion------\n")
+    putStr(bonita(simpl prop9))
